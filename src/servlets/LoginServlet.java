c63751ea63628aa6e1f1c221255a236687eb5e5f
Nikola Lisicic <nikola.lisicic@hotmail.com>
2015-11-16 18:30:35 +0100

    some minor editing

--- src/servlets/LoginServlet.java
+++ src/servlets/LoginServlet.java
@@ -1,6 +1,5 @@
 package servlets;
 
-
 import java.io.IOException;
 import java.util.List;
 
@@ -23,32 +22,39 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		UserDatabaseService uds = new UserDatabaseService();
-		User u = uds.validate(username, password); // find user in the database with passed username and password
+		User u = uds.validate(username, password); // find user in the database
+													// with passed username and
+													// password
 		// if there isnt a user with that username/password
-		if(u == null){
-			// set currentUser to null 
+		if (u == null) {
+			// set currentUser to null
 			request.getSession().setAttribute("currentUser", null);
 			// set the message to be displayed
-			request.getSession().setAttribute("message","Wrong username or password, try again!");
+			request.getSession().setAttribute("message", "Wrong username or password, try again!");
 			response.sendRedirect("login.jsp");
-		} 
+		}
 		// if there is a user in the database with that username and password
 		else {
 			// set user as current
 			request.getSession().setAttribute("currentUser", u);
-			DatabaseService ds = new DatabaseService();
-			List<Person> peopleList = ds.getAllPeople(); // get list of people if there was some changes before
-			request.getSession().setAttribute("peopleList", peopleList);
+
+			if (u.getRole().equals("admin")) {
+				DatabaseService ds = new DatabaseService();
+				List<Person> peopleList = ds.getAllPeople(); // get list of people for the admin menu
+				request.getSession().setAttribute("peopleList", peopleList);
+			}
 			response.sendRedirect("welcome.jsp");
-			
+
 		}
-		
+
 	}
 
 }
