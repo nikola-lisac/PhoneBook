a5e28c6d7ec19147cfe0171ece2f87a4fc41e146
Nikola Lisicic <nikola.lisicic@hotmail.com>
2015-11-21 20:58:58 +0100

    some minor change

--- src/dao/MyConnection.java
+++ src/dao/MyConnection.java
@@ -9,8 +9,6 @@ public class MyConnection {
 	private static final String USERNAME = "root";
 	// connection password
 	private static final String PASSWORD = "nikola";
-	// name of the database
-	private static final String DB = "phonebook";
 
 	/**
 	 * Method for connecting to the specified database
@@ -20,18 +18,19 @@ public class MyConnection {
 	 * @throws SQLException
 	 * 
 	 */
-	public static Connection connectToDb() throws SQLException {
+	public static Connection connectToDb() {
 		Connection connection = null;
 		try {
 			// mySQl driver
 			Class.forName("com.mysql.jdbc.Driver");
 			// connection with the database
 			connection = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/" + DB, USERNAME, PASSWORD);
+					"jdbc:mysql://localhost:3306/phonebook", USERNAME, PASSWORD);
 
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+		} catch (SQLException | ClassNotFoundException ex) {
+			ex.printStackTrace();
 		}
+		
 		return connection;
 	}
 }
